1fc834007f1d0f6cc1a3e649d9ca1c741b29e167
awokencreative <38005245+awokencreative@users.noreply.github.com>
2018-04-27 14:01:06 -0400

    Add files via upload

---
+++ Main HVAC/Coding/servicedispatcher.php
@@ -0,0 +1,58 @@
+<?php
+require('includes/prepend.php');
+$PAGE_TITLE='About Main Enterprises Inc. - Mechanical Contracting Company';
+$PAGE_KEYWORDS='mechanical contracting company, building energy audit fairfield county, energy reduction solutions fairfield county, energy incentives fairfield county, building audit fairfield county, alternative energy incentives fairfield county, 24/7 hvac service fairfield county, hvac repairs fairfield county';
+$PAGE_DESCRIPTION='Main Enterprises offers 24-hour service department, a sheet metal and mechanical pipe fabrication facility, our design/build engineering staff and an expert installation group. These services are supported by knowledgeable back-office management staff.';
+$PAGE_SCRIPT[]='';
+$PAGE_STYLE[]='';
+$PAGE_ONLOAD='';
+
+require('includes/top_template.php');
+?>
+<h3>We Are Hiring HVAC Service Dispatchers!<br> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYedwBTZhk1SP7Hj18ysKL3UpQ6M6VcW_8DEyWu7yxETxCTA/viewform?usp=sf_link"> <u>Click here to apply online!</u></a><h3>
+
+
+<h3>Job Title: HVAC Service Dispatcher<br>
+  <br>
+
+
+<h3>Primary Function:</h3>
+<p>Maintain the Service Technicians daily schedules and dispatch the technicians. Schedule return trips with Customers when parts are in. Maintain customer database with current information. Clear and concise communications with Department Managers, employees and company customers, including keeping customers apprised as to technician ETA, lead time for equipment and materials, and proposed schedule. Provide accounting with maintenance contract billing information. Prepare service invoices and timesheets for processing.</p>
+<p>Reports to: Service Manager</p>
+<p> <h3>Required Qualifications:</h3>
+    <ul>
+      <li>Advanced customer service skills</li>
+	    <li>Ability to multi-task</li>
+	    <li>Organized</li>
+	    <li>Geographical knowledge of service area</li>
+	    <li>Knowledge of HVAC Industry</li>
+	    <li>Computer skills</li>
+	    <li>High school diploma or general education degree (GED)</li>
+	    <li>Five years related experience and/or training in customer service, dispatching or equivalent combination of education and experience</li>
+    </ul>
+
+    <p><h4>Daily Duties:</h4></p>
+    <ul>
+      <li>Take incoming customer calls</li>
+      <li>Schedule and coordinate all service calls as they are received</li>
+      <li>Create dispatch work order for all service calls</li>
+      <li>Dispatch Service Technicians to pending calls</li>
+      <li>Dispatch Service Technicians from home to first call.</li>
+      <li>Debrief Service Technicians after completion of each call</li>
+      <li>Respond to all messages left overnight</li>
+      <li>Maintain the dispatch board / schedule</li>
+      <li>Forecast work load 1 – 2 weeks out</li>
+      <li>Contact customers with a Parts Pending status as parts are received for scheduling</li>
+      <li>Follow up on pending and recommended work with customers</li>
+      <li>Order parts for special orders</li>
+      <li>Computer skills</li>
+      <li>Update customer files as information is received from installation. (Extended warranties, equipment info, new customers, etc.</li>
+      <li>Maintain on call schedule, to include primary on call technician and backup</li>
+      <li>Maintain customer history files in database</li>
+      <li>Prepare paperwork for all manufacturer warranty parts</li>
+      <li>Facilitate return of warranty parts</li>
+      <li>Compare service timecards and work orders to service schedule</li>
+      <li>Check service time cards for accuracy and process payroll</li>
+      <li>Happy calls / customer surveys after service calls are completed</li>
+      <li>Other duties as assigned.</li>
+    </ul>
---
+++ Main HVAC/Coding/servicetechjobs.php
@@ -0,0 +1,69 @@
+<?php
+require('includes/prepend.php');
+$PAGE_TITLE='About Main Enterprises Inc. - Mechanical Contracting Company';
+$PAGE_KEYWORDS='mechanical contracting company, building energy audit fairfield county, energy reduction solutions fairfield county, energy incentives fairfield county, building audit fairfield county, alternative energy incentives fairfield county, 24/7 hvac service fairfield county, hvac repairs fairfield county';
+$PAGE_DESCRIPTION='Main Enterprises offers 24-hour service department, a sheet metal and mechanical pipe fabrication facility, our design/build engineering staff and an expert installation group. These services are supported by knowledgeable back-office management staff.';
+$PAGE_SCRIPT[]='';
+$PAGE_STYLE[]='';
+$PAGE_ONLOAD='';
+
+require('includes/top_template.php');
+?>
+<h3>We Are Hiring Service Technicians!<br> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYedwBTZhk1SP7Hj18ysKL3UpQ6M6VcW_8DEyWu7yxETxCTA/viewform?usp=sf_link"> <u>Click here to apply online!</u></a><h3>
+
+
+<h3>Job Title: Service Technician<br>
+  <br>
+
+
+<h3>Summary:</h3>
+<p>Licensed Journeymen with commercial experience that has the capabilities to perform start-up of new equipment, commissioning, warranty service, and preventative/diagnostic service. Will be responsible for all in-house commercial clients.  Responsible for inspecting, maintaining, troubleshooting, servicing, and repairing all major brands of commercial HVAC equipment. Work may include commissioning, start-up, adjusting, repair, modification, and/or replacement of HVAC equipment.
+
+<p> <h3>Accountabilities</h3>
+    <ol>
+      <li>Treat all people associated with Main Enterprises Inc. with respect, honesty and integrity at all times.</li>
+      <li>Conduct preventative maintenance of HVAC systems.</li>
+      <li>Troubleshoot and service HVAC systems and components (predictive)</li>
+      <li>Accurately estimate time and material requirements for repairs and replacements</li>
+      <li>Maintain a complete set of necessary tools and test equipment</li>
+      <li>Must be available to work overtime on a regular basis and must be able to respond to service calls as needed beyond normal business hours</li>
+      <li>Work as on-call technician during rotation period</li>
+      <li>Responsible for assets used on job site</li>
+      <li>Holds a valid driver’s license, CT Trade’s license, S-1,S-2, OSHA 10 card</li>
+      <li>Make purchases at local supply houses according to Company procedure</li>
+      <li>Complete all required paper work in a neat and timely manner</li>
+      <li>Other duties as assigned.</li>
+    </ol>
+
+  <p> <h3>Qualifications</h3> </p>
+
+      <ul>
+        <li>CT HVAC Journeyman’s license (S-2 preferred) </li>
+        <li>Valid OSHA 10 card (<5 years old for state of CT) </li>
+        <li>Minimum five years commercial service experience. </li>
+        <li>Experience in control systems, frequency drives, boilers, chillers, towers, mission critical cooling,
+Equipment rigging, safety requirements, and applicable local codes and regulations </li>
+
+        <li>Complete understanding of rooftop units, heat pumps, split systems, package units, hydronic systems, VAV Systems, central plant operations and all related control systems. </li>
+        <li>Knowledge of tools and equipment needed to complete a job. </li>
+        <li>Thorough knowledge and understanding of air conditioning fundamentals, refrigeration cycle, and
+Electricity as it relates to HVAC equipment to include proficiency in troubleshooting same. </li>
+        <li>Knowledge of commercial refrigeration systems a plus. </li>
+        <li>Ability to understand written sentences and paragraphs in work-related documents. </li>
+        <li>Able to use logic and reasoning to identify the strengths and weaknesses of solution and approaches to problems. </li>
+        <li>Able to perform mathematical calculations. </li>
+        <h4>Physical and Mental Job Requirements</h4>
+
+        <li>Must be able to lift up to 60 pounds</li>
+        <li>Spend long periods of time standing</li>
+        <li>Ability to climb ladders to perform work</li>
+        <li>Work in high elevations</li>
+
+        <h4>Working Conditions</h4>
+
+        <li>Daily travel to multiple job locations</li>
+        <li>May be subjected to inclement weather and hazardous working conditions. </li>
+        <li>Work weekend or night-time duty and on call duty as needed</li>
+      </ul>
+
+      <h4>NOTE:  It is understood that the above may be revised periodically based on Company needs at the discretion of the President.  We will discuss any such changes to assure a common understanding.</h4>
